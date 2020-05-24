f9bf1e971ca1cc2996dea38138a24feeccc7e307
kevinwere <werekevinongondo@gmail.com>
2020-05-24 14:18:12 +0300

    Completed the scroll behavior

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,10 @@
 import "../styles/styles.css";
-import MobileMenu from "./modules/mobileMenu";
+import MobileMenu from "./module/mobileMenu";
+import RevealOnScroll from "./module/RevealOnScroll";
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
+new RevealOnScroll(document.querySelectorAll(".testimonials"), 60);
 let mobileMenu = new MobileMenu();
+
 if (module.hot) {
   module.hot.accept();
 }
