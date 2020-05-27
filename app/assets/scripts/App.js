998fb7c58427fe2a70869759559704095179a843
kevinwere <werekevinongondo@gmail.com>
2020-05-27 03:04:51 +0300

    Completed the sticky header section

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,11 @@
 import "../styles/styles.css";
 import MobileMenu from "./module/mobileMenu";
 import RevealOnScroll from "./module/RevealOnScroll";
+import StickyHeader from "./module/StickyHeader";
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonials"), 60);
 let mobileMenu = new MobileMenu();
+let stickyHeader = new StickyHeader();
 
 if (module.hot) {
   module.hot.accept();
